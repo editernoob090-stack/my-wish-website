4a327035bdd54d872bb6905ab19c055a2626719f
editernoob090-stack <editernoob090@gmail.com>
2026-09-06 20:26:30 +0545

    Initialize package.json for wish-website project

---
+++ server.js
@@ -0,0 +1,13 @@
+{
+  "name": "wish-website",
+  "version": "1.0.0",
+  "description": "Way to make your wish true website",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "ws": "^8.17.0"
+  }
+    }
